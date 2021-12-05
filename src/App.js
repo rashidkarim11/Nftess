162543329cf2ca1ef057bb95401f777cb81370ad
Nouman Azam <noumanhunzai222@gmail.com>
2021-12-06 02:07:51 +0500

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import React from "react";
+import Header from './components/Header';
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import './App.css';
+// import componenets for routing
+import Home from './pages/Home';
+import Browse from './pages/Browse';
+import Artwork from './pages/Artwork';
+import Shop from './pages/Shop';
+
 const App = () => {
-  return <h1>App</h1>;
+  return (
+     <>
+      <Router>
+        <Header/>
+        <Routes>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/artwork' element={<Artwork/>}/>
+            <Route path='/browse' element={<Browse/>}/>
+            <Route path='/shop' element={<Shop/>}/>
+        </Routes>
+      </Router>
+      
+     </>
+  );
 };
 
 export default App;
---
+++ src/pages/Browse/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Browse = () => {
+    return (
+        <div>
+            <h3>Browse</h3>
+        </div>
+    )
+}
+
+export default Browse
