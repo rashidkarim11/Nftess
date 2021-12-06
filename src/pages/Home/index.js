dc160f05452d63692985794b8b1eaa53bec3aafb
Nouman Azam <noumanhunzai222@gmail.com>
2021-12-06 13:41:51 +0500

    Added NftMarketPlace Section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import NftMarketplaceHome from "../../Component/ArtWorkSection/Artwork";
+import NftMarketplaceHome from "../../Component/marketPlaceHome/NftMarketplaceHome";
 import Artwork from "../../Component/ArtWorkSection/Artwork";
 import Card from "../../Component/NftCard/NftCard" ;
 import EconomySection from "../../Component/EconomySection"
 const Home = () => {
     return (
         <>
-            <NftMarketplaceHome />
+            <NftMarketplaceHome/>
             <Artwork />
             <Card />
             <EconomySection/>
