d291eed20795ef9ae65af73ea2ca6fc19f54ad1f
Rehan-AF <af.rehan03@gmail.com>
2021-12-07 17:07:02 +0500

    chnages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react";
 import "@fontsource/roboto";
 import App from "./App";
-
 
 ReactDOM.render(<App />, document.querySelector("#root"));
