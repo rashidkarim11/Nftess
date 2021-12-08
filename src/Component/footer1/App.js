23b5af52ca06b021631ebc58c5900cd57d7db274
ayoubkhan123 <ayubkhan666304@gmail.com>
2021-12-07 18:28:48 -0800

    footer include

---
+++ src/Component/footer1/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="footer-container">
+    <div className="footer-head"><h1>Some count that matters</h1>
+    <p>Our achievement in the journey depicted in numbers</p>
+    </div>
+        <div className="footer-num">
+        <div className="row1">
+        <h2>50000</h2>
+        <p>Products</p></div>
+
+        <div className="row2">
+        <h2>31000</h2>
+        <p>Creaters</p></div>
+
+        <div className="row3">  <h2>50000</h2>
+        <p>Buy/Sell</p></div>
+
+        <div className="row4">   <h2>8</h2>
+        <p>Years of Journey</p></div>
+        
+
+
+
+
+        </div>
+
+
+
+    </div>
+
+    </>
+  );
+}
+
+export default App;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import NftMarketplaceHome from "../../Component/marketPlaceHome/NftMarketplaceHome";
 import Artwork from "../../Component/ArtWorkSection/Artwork";
 import EconomySection from "../../Component/EconomySection";
+
 const Home = () => {
   return (
     <>
       <NftMarketplaceHome />
       <Artwork />
       <EconomySection />
+     
+
     </>
   );
 };
